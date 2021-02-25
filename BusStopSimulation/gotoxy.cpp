490f021e1d79d48d016ed67b1528fc4e3f025401
Zahar44 <3axapduradashu@gmail.com>
2021-02-25 15:34:54 +0200

    Added gotoxy function

---
+++ BusStopSimulation/gotoxy.cpp
@@ -0,0 +1,8 @@
+#include "gotoxy.h"
+void gotoxy(int xpos, int ypos)
+{
+	COORD scrn;
+	HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
+	scrn.X = xpos; scrn.Y = ypos;
+	SetConsoleCursorPosition(hOuput, scrn);
+}
